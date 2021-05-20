962a8fb283a7a4a2b1a31d36be82fb4352ed3ee9
Carlos Augusto <augustino2003@hotmail.com>
2021-05-20 16:20:20 -0300

    Ajustes na API para trazer Status e Prioridade da OS

--- src/main/java/com/carlos/os/dtos/OSDTO.java
+++ src/main/java/com/carlos/os/dtos/OSDTO.java
@@ -8,6 +8,8 @@ import javax.validation.constraints.NotEmpty;
 import com.carlos.os.domain.Cliente;
 import com.carlos.os.domain.OS;
 import com.carlos.os.domain.Tecnico;
+import com.carlos.os.domain.enums.Prioridade;
+import com.carlos.os.domain.enums.Status;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class OSDTO implements Serializable {
@@ -71,8 +73,8 @@ public class OSDTO implements Serializable {
 		this.dataFechamento = dataFechamento;
 	}
 
-	public Integer getPrioridade() {
-		return prioridade;
+	public Prioridade getPrioridade() {
+		return Prioridade.toEnum(this.prioridade);
 	}
 
 	public void setPrioridade(Integer prioridade) {
@@ -87,8 +89,8 @@ public class OSDTO implements Serializable {
 		this.observacoes = observacoes;
 	}
 
-	public Integer getStatus() {
-		return status;
+	public Status getStatus() {
+		return Status.toEnum(this.status);
 	}
 
 	public void setStatus(Integer status) {
